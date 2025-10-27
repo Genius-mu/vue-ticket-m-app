23a4811f0cc8da95e77d93b7100d3bcae1655cb5
genius mu <adegbitemustapha73@gmail.com>
2025-10-27 23:15:04 +0100

    vueApp

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,14 +1,14 @@
 <template>
   <section class="px-6 py-10 min-h-screen bg-gradient-to-b from-blue-50 to-blue-100">
     <div class="flex justify-between items-center mb-6">
-      <h1 class="xl:text-3xl md:text-3xl sm:text-3xl text-xl font-bold text-blue-700">Ticket Dashboard</h1>
+      <h1 class="xl:text-3xl md:text-3xl sm:text-3xl text-[17px] font-bold text-blue-700">Ticket Dashboard</h1>
       <button @click="logout" class="bg-blue-600 hover:bg-blue-700 text-white px-5 py-2 rounded-lg">
         Logout
       </button>
     </div>
 
     <!-- Summary Cards -->
-    <div class="grid grid-cols-2 md:grid-cols-4 gap-4 mb-6">
+    <div class="grid sm:grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mb-6 ">
       <div class="bg-white rounded-xl shadow-md p-4 text-center border border-blue-100">
         <h3 class="text-gray-600">Total</h3>
         <p class="text-2xl font-bold text-blue-700">{{ total }}</p>
@@ -29,7 +29,7 @@
 
     <RouterLink
       to="/tickets"
-      class="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg font-medium shadow-md"
+      class="bg-blue-600 w-full flex justify-center sm:w-fit hover:bg-blue-700 text-white px-6 py-3 rounded-lg font-medium shadow-md"
     >
       Go to Tickets Page →
     </RouterLink>
